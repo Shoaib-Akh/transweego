06e97d0ae3d11fb3cde9863646bd0b4019c5077d
shoaib akhtar <shoib2738248@gmail.com>
2024-06-05 23:12:06 +0500

    create landing page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from './Screen/Auth/login';
 import SignUp from './Screen/Auth/signUp';
 import ComponySignUp from './Screen/Auth/signUp/ComponySignUp';
 import AddDriver from './Screen/Auth/signUp/ComponySignUp/AddDriver';
+import LandingPage from './Screen/website/LandingPage';
 
 function App() {
   return (
@@ -20,11 +21,7 @@ function App() {
         <Route path='signUp' element={<SignUp />} />
         <Route path='ComponySignUp' element={<ComponySignUp />} />
         <Route path='AddDriver' element={<AddDriver />} />
-
-
-
-
-
+        <Route path='LandingPage' element={<LandingPage />} />
 
       </Routes>
     </div>
---
+++ src/Screen/website/LandingPage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../style.scss";
+
+
+
+
+const LandingPage = () => {
+  return (
+    <>
+    <h1>cdasfcad</h1>
+    </>
+  );
+};
+
+export default LandingPage;
