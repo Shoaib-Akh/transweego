830ad51fb4ac1231683b124063aa4eda29be4a1c
shoaib AKhtar <shoib2738248@gmail.com>
2024-06-13 16:51:00 +0500

    prelaunch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import logo from './logo.svg';
 import './App.scss';
-
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './Screen/website/HomePage';
-import HowItWork from './Screen/website/HowItWork';
-import Login from './Screen/Auth/login';
 import SignUp from './Screen/Auth/signUp';
 import CompanySignup from './Screen/Auth/signUp/CompanySignup';
-import AddDriver from './Screen/Auth/signUp/CompanySignup/AddDriver';
 import LandingPage from './Screen/website/LandingPage';
 import IndividualSignUp from './Screen/Auth/signUp/IndividualSignUp';
 import IndividualTransporterSignUp from './Screen/Auth/signUp/IndividualTransporterSignUp';
@@ -15,19 +9,12 @@ import IndividualTransporterSignUp from './Screen/Auth/signUp/IndividualTranspor
 function App() {
   return (
     <div className="App">
-      {/* <Navbar/> */}
       <Routes>
       <Route path='/' element={<LandingPage />} />
-      
         <Route path='individual-transporter-SignUp' element={<IndividualTransporterSignUp />} />
-        <Route path='HowItWork' element={<HowItWork />} />
         <Route path='individual-signUp' element={<IndividualSignUp/>} />
-
-        <Route path='login' element={<Login />} />
         <Route path='signup' element={<SignUp />} />
         <Route path='company-signup' element={<CompanySignup />} />
-        <Route path='AddDriver' element={<AddDriver />} />
-
       </Routes>
     </div>
   );
--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
-import userReducer from './api/loginSlice';
+import userReducer from './api/IndividualSignUpSlice'
 import serviceTypesReducer from './api/getServicesSlice';
 import GetGenderReducer from './api/GetGenderSlice';
 
