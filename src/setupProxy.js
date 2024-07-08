38b44affe3048675b785fcb6bab6944a1c8f441d
shoaib AKhtar <shoib2738248@gmail.com>
2024-07-08 14:04:56 +0500

    set the drop dwon

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://54.157.229.132:5000',
+      changeOrigin: true,
+    })
+  );
+};
